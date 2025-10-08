5c1cddb8745f09a462b25b37f98ba3ab4958170c
Roshan1718jr <156595736+Roshan1718jr@users.noreply.github.com>
2025-10-08 20:52:20 +0530

    add task1.py

---
+++ task3.py
@@ -0,0 +1,9 @@
+num = int(input("enter your number :"))
+if num%3==0:
+    print("fizz")
+elif num%5==0:
+    print("buzz")
+elif num%4==0:
+    print("fizz buzz")
+else:
+    print("enter the vaild number ")
