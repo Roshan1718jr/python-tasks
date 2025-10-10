0adf19d603888bfc1cf57dd538470b8f67e731e6
Roshan <156595736+Roshan1718jr@users.noreply.github.com>
2025-10-10 16:58:37 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,32 @@
+for i in range(-10,1,1):
+  print(i)
+#while
+a=6
+n=1
+while n<a:
+  print("py")
+  n+=1
+print()  
+#while count 5t
+n=5
+a=1
+while a<=n:
+  print("python")
+  a+=1
+print()  
+#while use break
+lengh=10
+a=1
+while a<=lengh:
+  print("hello")
+  if a==7:
+     break
+  a+=1
+#fact
+n=int(input("enter the number "))
+fact=1
+for i in range(1,n+1):
+    fact*=i
+print("the fact",fact)    
+ 
+
