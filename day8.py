fbc8b21b795b9d08ac803d5530e0d1153dc35fdf
Roshan <156595736+Roshan1718jr@users.noreply.github.com>
2025-10-15 17:06:56 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,24 @@
+#task1
+n=int(input("enter the number"))
+m=0
+while n>0:
+    m+=n
+    n=int(input("enter the number"))
+print(m)
+#task2
+li=[44,66,-11,55]
+a=[]
+b=[]
+for i in li:
+  if i>0:
+      a.append(i)
+  else:
+      b.append(i)
+print(li)      
+print(a)
+print(b)
+    
+    
+    
+        
+        
